1bac8cba689c031878a5431345510c5d0f10ecaf
Korey-ops <22004@karamu.nz>
2025-06-29 21:12:05 +1200

    Romoving joke function

--- Login.js
+++ Login.js
@@ -5,6 +5,5 @@ var button = new ej.buttons.Button({cssClass: 'e-link'});
 button.appendTo('#element');
 
 button.element.onclick  = function () {
-    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
     location.href = 'home.html'
 }
